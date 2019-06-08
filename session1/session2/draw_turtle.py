d82fed82163aa323d4c84dd1e737d4a40128c7b2
Tuansushi2005 <tuansushi05@gmail.com>
2019-06-08 19:01:04 +0700

    tuan 1st commit

---
+++ session1/session2/draw_turtle.py
@@ -0,0 +1,13 @@
+from turtle import *
+shape("turtle")
+speed(-1)
+for i in range (10):
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(45)
+mainloop()
---
+++ session1/session2/variable.py
@@ -0,0 +1,16 @@
+print("learning variable")
+name = "Anh Tuan"       
+print("name: ", type(name))
+
+age = 20
+print("age: ",age)
+print("type age: ",type(age))
+
+name = input("enter your name: ")
+print(name)
+
+year_born = int(input("enter your year of birht: "))
+year_current = int(input("enter current year: "))
+age = int(year_current) - int(year_born)
+print("age: ",age)
+
